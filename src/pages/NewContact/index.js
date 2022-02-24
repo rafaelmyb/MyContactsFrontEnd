079f51df4dfb12d001f02d7a0431b3f1947b3ae2
rafaelmyb <rafaelyamada321@gmail.com>
2022-02-23 20:26:51 -0400

    add input & select components

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,7 +1,15 @@
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 export default function NewContact() {
   return (
-    <PageHeader title="Novo Contato" />
+    <>
+      <PageHeader title="Novo Contato" />
+      <Input placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+      </Select>
+    </>
   );
 }
