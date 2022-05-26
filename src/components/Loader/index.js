7243f3e118ee8cdcd3ec2f7e2fc4a9c23de99591
rafaelmyb <rafaelyamada321@gmail.com>
2022-05-26 18:03:17 -0400

    add rendering contacts in Home

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,9 +1,19 @@
+import PropTypes from 'prop-types';
+
 import { Overlay } from './styles';
 
-export default function Loader() {
+export default function Loader({ isLoading }) {
+  if (!isLoading) {
+    return null;
+  }
+
   return (
     <Overlay>
       <div className="loader" />
     </Overlay>
   );
 }
+
+Loader.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+};
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -18,7 +18,7 @@ export const InputSearchContainer = styled.div`
     padding: 0 16px;
 
     &::placeholder {
-      color: #BCBCBC;
+      color: #bcbcbc;
     }
   }
 `;
@@ -49,23 +49,26 @@ export const Header = styled.header`
   }
 `;
 
-export const ListContainer = styled.div`
+export const ListHeader = styled.header`
   margin-top: 24px;
 
-  header {
-    margin-bottom: 8px;
+  margin-bottom: 8px;
 
-    button {
-      background: transparent;
-      border: none;
-      display: flex;
-      align-items: center;
+  button {
+    background: transparent;
+    border: none;
+    display: flex;
+    align-items: center;
 
-      span {
-        margin-right: 8px;
-        font-weight: bold;
-        color: ${({ theme }) => theme.colors.primary.main};
-      }
+    span {
+      margin-right: 8px;
+      font-weight: bold;
+      color: ${({ theme }) => theme.colors.primary.main};
+    }
+
+    img {
+      transform: ${({ orderBy }) => (orderBy === 'asc' ? 'rotate(180deg)' : 'rotate(0deg)')};
+      transition: transform 0.2s ease-in;
     }
   }
 `;
